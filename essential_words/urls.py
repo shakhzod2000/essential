208c0c53375fb11820f76f1a6e84434dc1da14b7
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-08-14 23:50:16 +0500

    Add files via upload

---
+++ essential_words/urls.py
@@ -0,0 +1,18 @@
+# essential_words/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from quiz import views
+from django.conf import settings
+from django.conf.urls import include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('quiz.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include('debug_toolbar.urls'))
+    ] + urlpatterns
