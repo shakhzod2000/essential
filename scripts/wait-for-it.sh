322c13afc4f900b03dfee55d7b1f251d5ec92e88
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-08-16 20:19:24 +0500

    Update wait-for-it.sh

--- scripts/wait-for-it.sh
+++ scripts/wait-for-it.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 # wait-for-it.sh
-
-set -e
-
-host="$1"
-shift
-cmd="$@"
-
-until nc -z "$host" 3306; do
-  echo "Waiting for MySQL..."
-  sleep 1
+until nc -z -v -w30 db 3306
+do
+  echo "Waiting for MySQL connection..."
+  sleep 5
 done
 
->&2 echo "MySQL is up - executing command"
-exec $cmd
+echo "MySQL is up and running!"
+exec "$@"
